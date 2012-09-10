3e27381ab1efc28cd74c28b07f1fe8c887bf8f9a
fjodorekstromjayway <fjodor.ekstrom@jayway.com>
2012-09-10 13:39:04 +0200

    added new tests and a new function method

--- spec/HelloWorld_spec.js
+++ spec/HelloWorld_spec.js
@@ -3,3 +3,52 @@ describe("Hello World", function(){
 		expect(helloWorld()).toEqual("Hello World!");
 	});
 });
+
+describe("Person", function(){
+	it("calls the sayHello() function", function(){
+		var fakePerson = new Person();
+		spyOn(fakePerson, "sayHello");
+		fakePerson.helloSomeone("world");
+		expect(fakePerson.sayHello).toHaveBeenCalled();
+	});
+
+	it("greets the world", function(){
+		var fakePerson = new Person();
+		spyOn(fakePerson, "helloSomeone");
+		fakePerson.helloSomeone("world");
+		expect(fakePerson.helloSomeone).toHaveBeenCalledWith("world");
+	});
+});
+
+describe("NewFunc", function(){
+	it("returns true if paramVal is 1", function(){
+		var fakeFunc = new newFunc();
+		spyOn(fakeFunc, 'sendTrueOrFalse');
+		fakeFunc.sendTrueOrFalse(1);
+		expect(fakeFunc.sendTrueOrFalse).toEqual(true);
+	});
+
+	it("returns false if paramVal is not 1", function(){
+		var fakeFunc = new newFunc();
+		var fakeParam = Math.floor(Math.random()*100)+1;
+		spyOn(fakeFunc, 'sendTrueOrFalse');
+		fakeFunc.sendTrueOrFalse(2);
+		expect(fakeFunc.sendTrueOrFalse).toEqual(false);
+	});
+
+	it("is called with the correct parameter", function(){
+		var fakeFunc = new newFunc();
+		spyOn(fakeFunc, "sendTrueOrFalse");
+		fakeFunc.sendTrueOrFalse("testaar");
+		expect(fakeFunc.sendTrueOrFalse).toHaveBeenCalledWith("testaar");
+		expect(fakeFunc.sendTrueOrFalse).not.toHaveBeenCalledWith("tes");
+	});
+
+	it("calls the sendTrueOrFalse method", function(){
+		var fakeFunc = new newFunc();
+		spyOn(fakeFunc, "sendTrueOrFalse");
+		fakeFunc.sendTrueOrFalse();
+		expect(fakeFunc.sendTrueOrFalse).toHaveBeenCalled();
+	});
+
+});
--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,3 +1,26 @@
 function helloWorld(){
 	return "Hello World!";
-}
+};
+
+var Person = function(){};
+
+Person.prototype.helloSomeone = function(toGreet){
+	return this.sayHello() + " " + toGreet;
+};
+
+Person.prototype.sayHello = function(){
+	return "Hello";
+};
+
+var newFunc = function(){};
+
+newFunc.prototype.sendTrueOrFalse = function(paramVal){
+	var result = true;
+	if(paramVal === 1){
+		result = true;
+	} else {
+		result = false;
+	}
+	return result;
+};
+
